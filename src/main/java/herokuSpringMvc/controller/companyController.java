1319ac135b659e13a948a167480c08d415ba31ed
aditya <aditya@addy>
2017-11-03 23:54:12 +0530

    added login page as main pg

--- src/main/java/herokuSpringMvc/controller/companyController.java
+++ src/main/java/herokuSpringMvc/controller/companyController.java
@@ -10,7 +10,7 @@ public class companyController {
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String index()
 	{
-		return "index";
+		return "login";
 	}
 	
 	@RequestMapping(value="/welcome", method=RequestMethod.GET)
